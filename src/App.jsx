547d26fdf22fe07527fd302ca6e406abaa822373
Yams Shaik <132766523+YamsShaik@users.noreply.github.com>
2026-06-25 08:26:38 +0530

    Change text from 'twenty-four' to 'Happy-Birthday'

--- src/App.jsx
+++ src/App.jsx
@@ -426,7 +426,7 @@ export default function App() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.2 }}
               >
-                ✦ twenty-four ✦
+                ✦ Happy-Birthday ✦
               </motion.span>
 
               <motion.h1
